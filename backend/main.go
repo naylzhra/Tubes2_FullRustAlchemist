c0f898a9a2437a523da0eecfbc6e3268a4927c51
Syahrizal Khairan <13523063@std.stei.itb.ac.id>
2025-04-23 11:15:07 +0700

    feat: graph data structure

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,30 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	scraping.ScrapeRecipes()
+	err := scraping.ScrapeRecipes()
+	if err != nil {
+		panic(err)
+	}
+	recipes, err := scraping.GetScrapedRecipesJSON()
+	if err != nil {
+		panic(err)
+	}
+
+	// Initialize the recipe graph
+	graph := RecipeGraph{Elements: make([]ElementNode, 0)}
+	err = ConstructRecipeGraph(recipes, &graph)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Recipe graph constructed successfully!")
+	fmt.Println("Length of element nodes:", len(graph.Elements))
+	total_edges := 0
+	for _, element := range graph.Elements {
+		// fmt.Printf("Element %d: %s\n", i, element.Name)
+		// fmt.Printf("Number of children: %d\n", len(element.Children))
+		// fmt.Printf("Number of recipes: %d\n", len(element.Recipes))
+		total_edges += len(element.Recipes)
+	}
+	fmt.Println("Total number of edges:", total_edges)
 }
