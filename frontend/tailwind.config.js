319dda9c59692334d91ccefe6b58d92f188a7a28
Syahrizal Khairan <13523063@std.stei.itb.ac.id>
2025-05-07 08:21:11 +0700

    refactor: NEXT project structure and transition to typescript and tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Scan all files in the src directory
+    "./pages/**/*.{js,ts,jsx,tsx}", // Scan all files in the pages directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
