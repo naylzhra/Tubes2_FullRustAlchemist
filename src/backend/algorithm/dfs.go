d5eabea699daf9e22f65bc818fafc603d6ae8250
Syahrizal Khairan <13523063@std.stei.itb.ac.id>
2025-05-12 01:04:26 +0700

    fix: multithreading multiple recipe DFS with no deadlock

--- src/backend/algorithm/dfs.go
+++ src/backend/algorithm/dfs.go
@@ -9,15 +9,9 @@ import (
 	"sync"
 )
 
-type SearchStatus struct {
-	result         chan int
-	continueSignal chan struct{}
-}
-
 type ResultTree struct {
-	mu             sync.Mutex
-	path           []*Recipe
-	remainingPaths int
+	mu   sync.Mutex
+	path []*Recipe
 }
 
 type Recipe struct {
@@ -33,9 +27,12 @@ func DFS(target *search.ElementNode, graph *search.RecipeGraph, maxPaths int, no
 		return []string{ParseCraftingPath(result, 1, graph)}
 	}
 
-	// Multithreaded multiple recipe DFS
-	resultJSONs := make([]string, 0)
-	return resultJSONs
+	return findMultiplePaths(target, graph, maxPaths, nodeVisited)
+}
+
+func mergeTree(tree0 *ResultTree, tree1 *ResultTree, resulto *ResultTree) {
+	resulto.path = append(resulto.path, tree0.path...)
+	resulto.path = append(resulto.path, tree1.path...)
 }
 
 /* ----------------------------------------- Single Recipe DFS ----------------------------------------------- */
@@ -83,112 +80,138 @@ func findSinglePath(target *search.ElementNode, graph *search.RecipeGraph, resul
 	return nil
 }
 
-func mergeTree(tree0 *ResultTree, tree1 *ResultTree, resulto *ResultTree) {
-	resulto.path = append(resulto.path, tree0.path...)
-	resulto.path = append(resulto.path, tree1.path...)
-}
-
 /* ----------------------------------------- Multiple Recipe DFS ----------------------------------------------- */
 
-func findPath(target *search.ElementNode, graph *search.RecipeGraph, result *ResultTree, status SearchStatus, prevs []*search.ElementNode) {
-	// defer func() {
-	// 	completePathSignal <- 0
-	// }()
-
-	// // Base case: if the target is a base element, return
-	// if slices.Contains(graph.BaseElements, target) {
-	// 	result.mu.Lock()
-	// 	for _, elem := range result.paths {
-	// 		if elem.Name == target.Name {
-	// 			completePathSignal <- elem.ID
-	// 		}
-	// 	}
-	// 	result.mu.Unlock()
-	// 	<-completePathSignal
-	// 	return
-	// }
-
-	// fmt.Println("Searching for:", target.Name)
-
-	// // Generate thread for the two parents for a recipe (if not already created)
-	// // Wait until the recipe is fully complete before checking another recipe of this node
-	// for _, recipe := range target.Recipes {
-	// 	if slices.Contains(graph.BaseElements, recipe[0]) && slices.Contains(graph.BaseElements, recipe[1]) {
-	// 		result.mu.Lock()
-	// 		firstID := -1
-	// 		secondID := -1
-	// 		for _, elem := range result.paths {
-	// 			if elem.Name == recipe[0].Name {
-	// 				firstID = elem.ID
-	// 			}
-	// 			if elem.Name == recipe[1].Name {
-	// 				secondID = elem.ID
-	// 			}
-	// 		}
-	// 		newID := addResult(result, target, firstID, secondID)
-	// 		result.mu.Unlock()
-	// 		completePathSignal <- newID
-	// 		continueSearch := <-completePathSignal
-	// 		if continueSearch == 0 {
-	// 			break
-	// 		} else {
-	// 			continue
-	// 		}
-	// 	}
-
-	// 	if recipe[0].Tier >= target.Tier || recipe[1].Tier >= target.Tier {
-	// 		continue
-	// 	}
-
-	// 	// Decide whether to search for the parents of this element
-	// 	if pathAlreadyContains(prevs, recipe[0]) || pathAlreadyContains(prevs, recipe[1]) {
-	// 		continue
-	// 	}
-
-	// 	// Create a new thread for each parent of a recipe
-	// 	currentPath := slices.Clone(prevs)
-	// 	currentPath = append(currentPath, target)
-	// 	searchStatus := SearchStatus{
-	// 		pathSignal0: make(chan int),
-	// 		pathSignal1: make(chan int),
-	// 	}
-
-	// 	// Let the first parent backtrack, then the second
-	// 	// This is to enumerate all paths to recipe[0] x all paths to recipe[1]
-	// 	go findPath(recipe[0], graph, result, searchStatus.pathSignal0, currentPath)
-	// 	go findPath(recipe[1], graph, result, searchStatus.pathSignal1, currentPath)
-	// 	status0 := <-searchStatus.pathSignal0
-	// 	status1 := <-searchStatus.pathSignal1
-	// 	for status1 != 0 && status0 != 0 {
-	// 		if status0 == 0 { // First parent is done, so backtrack the second parent and restart the first parent thread
-	// 			searchStatus.pathSignal1 <- 1
-	// 			go findPath(recipe[0], graph, result, searchStatus.pathSignal0, currentPath)
-	// 			status0 = <-searchStatus.pathSignal0
-	// 			status1 = <-searchStatus.pathSignal1
-	// 		} else if status0 == 1 && status1 == 1 {
-	// 			result.mu.Lock()
-	// 			newID := addResult(result, target, status0, status1)
-	// 			result.mu.Unlock()
-	// 			completePathSignal <- newID
-
-	// 			continueSearch := <-completePathSignal
-	// 			if continueSearch == 0 {
-	// 				searchStatus.pathSignal0 <- 0
-	// 				searchStatus.pathSignal1 <- 0
-	// 				break
-	// 			} else {
-	// 				// Continue searching for the next recipe
-	// 				searchStatus.pathSignal0 <- 1
-	// 				status0 = <-searchStatus.pathSignal0
-	// 			}
-	// 		}
-	// 	}
-	// 	// Kill this routine
-	// }
+type SearchStatus struct {
+	result         chan int
+	continueSignal chan int
 }
 
-func pathAlreadyContains(prevs []*search.ElementNode, elem *search.ElementNode) bool {
-	return slices.Contains(prevs, elem)
+type SearchStatistic struct {
+	mu             sync.Mutex
+	remainingPaths int
+	nodeVisited    int
+}
+
+func findMultiplePaths(target *search.ElementNode, graph *search.RecipeGraph, maxPaths int, nodeVisited *int) []string {
+	resultJSONs := make([]string, 0, maxPaths)
+
+	status := SearchStatus{
+		result:         make(chan int),
+		continueSignal: make(chan int),
+	}
+	stats := &SearchStatistic{
+		mu:             sync.Mutex{},
+		remainingPaths: maxPaths,
+		nodeVisited:    0,
+	}
+	result := &ResultTree{path: make([]*Recipe, 0)}
+
+	go findPath(target, graph, result, status, stats)
+
+	counter := 0
+	condition := <-status.result
+	for condition != 0 {
+		counter++
+
+		newFilename := ParseCraftingPath(result, counter, graph)
+		resultJSONs = append(resultJSONs, newFilename)
+
+		if counter >= maxPaths {
+			status.continueSignal <- 0
+			<-status.result
+			break
+		} else {
+			status.continueSignal <- 1
+			condition = <-status.result
+		}
+	}
+
+	stats.mu.Lock()
+	*nodeVisited = stats.nodeVisited
+	stats.mu.Unlock()
+	return resultJSONs
+}
+
+func findPath(target *search.ElementNode, graph *search.RecipeGraph, result *ResultTree, status SearchStatus, stats *SearchStatistic) {
+	stats.mu.Lock()
+	stats.nodeVisited++
+	stats.mu.Unlock()
+
+	// Base case: if the target is a base element, return
+	if slices.Contains(graph.BaseElements, target) {
+		baseRecipe := &Recipe{element: target}
+		baseRecipe.composition = []*Recipe{baseRecipe, baseRecipe}
+		result.mu.Lock()
+		result.path = append(result.path, baseRecipe)
+		result.mu.Unlock()
+
+		status.result <- 1
+		<-status.continueSignal
+		status.result <- 0
+		return
+	}
+	if target.Name == "Time" {
+		status.result <- 0
+		return
+	}
+
+	for _, recipe := range target.Recipes {
+		if recipe[0].Tier >= target.Tier || recipe[1].Tier >= target.Tier {
+			continue
+		}
+
+		status0 := SearchStatus{result: make(chan int), continueSignal: make(chan int)}
+		result0 := &ResultTree{path: make([]*Recipe, 0)}
+		go findPath(recipe[0], graph, result0, status0, stats)
+
+		status1 := SearchStatus{result: make(chan int), continueSignal: make(chan int)}
+		result1 := &ResultTree{path: make([]*Recipe, 0)}
+		go findPath(recipe[1], graph, result1, status1, stats)
+
+		condition0 := <-status0.result
+		condition1 := <-status1.result
+		for condition0 != 0 && condition1 != 0 {
+			if condition0 == 1 && condition1 == 1 {
+				recipe := &Recipe{
+					element:     target,
+					composition: []*Recipe{result0.path[0], result1.path[0]},
+				}
+				result.mu.Lock()
+				result.path = make([]*Recipe, 0)
+				result.path = append(result.path, recipe)
+				mergeTree(result0, result1, result)
+				result.mu.Unlock()
+
+				status.result <- 1
+				continueSearch := <-status.continueSignal
+
+				if continueSearch == 0 {
+					status0.continueSignal <- 0
+					status1.continueSignal <- 0
+					<-status0.result
+					<-status1.result
+
+					break
+				} else {
+					status0.continueSignal <- 1
+					condition0 = <-status0.result
+				}
+			} else if condition0 == 0 {
+				status1.continueSignal <- 0
+
+				status0 = SearchStatus{result: make(chan int), continueSignal: make(chan int)}
+				result0 = &ResultTree{path: make([]*Recipe, 0)}
+				go findPath(recipe[0], graph, result0, status0, stats)
+
+				condition0 = <-status0.result
+				condition1 = <-status1.result
+			}
+		}
+	}
+
+	status.result <- 0
+	// Kill this routine
 }
 
 /* ----------------------------------------- Parse Search Output ----------------------------------------------- */
